beeb96d17c5ab9c6cb1884f24a3bae545e7e818f
Maruf Murshed <marufmurshed9255@gmail.com>
2024-10-17 15:01:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const usersRoutes = require("./routes/users");
+const reservationsRoutes = require("./routes/reservations");
+const jobsRoutes = require("./routes/jobs");
+
+// express app
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/users", usersRoutes);
+app.use("/api/reservations", reservationsRoutes);
+app.use("/api/jobs", jobsRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Primitivo-eats is Running Successfully");
+});
+app.get("/api", (req, res) => {
+  res.send("Primitivo-eats is Running Successfully");
+});
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    dbName: "primitivo-eats",
+  })
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        "connected to db & listening on port",
+        process.env.PORT || 5000
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
